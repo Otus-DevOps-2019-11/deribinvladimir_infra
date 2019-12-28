d26b9cc97374ee6a01d46cd8743218bf80913f82
Vladimir Deribin <technician@gmx.co.uk>
2019-12-28 22:40:18 +0300

    Add config files and scripts for reddit-full image

---
+++ packer/files/deploy_mc.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# add universe repocitory to install midnight commander
+sudo add-apt-repository universe
+sudo apt update
+
+# install mc
+sudo apt install mc
+
+# remove repo if need
+# sudo add-apt-repository --remove universe
---
+++ packer/files/deploy_puma.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# Install git
+sudo apt update
+sudo apt install git -y
+
+# yankee doodle going home
+cd ~
+
+# download src
+git clone -b monolith https://github.com/express42/reddit.git
+
+# install dependencies
+cd reddit && bundle install
+
+#create systemd config
+sudo touch /etc/systemd/system/puma.service
+sudo chmod 664 /etc/systemd/system/puma.service
+sudo echo -e "[Unit]\nDescription=Puma HTTP Server\nAfter=network.target\n[Service]\nType=simple\nExecStart=/usr/local/bin/puma -D\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n"
+
+# Reread configs
+sudo systemctl daemon-reload
+
+# Make autostart on boot
+sudo systemctl enable puma.service
+
+# Start app
+sudo systemctl start puma.service
+
+# Check status
+sudo systemctl status puma.service
+
+# oldway
+# start app
+# puma -d
+# check status
+# ps aux | grep puma
