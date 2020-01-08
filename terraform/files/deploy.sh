99d55aff55a19c5ed9c981edd9f3e864a3b05924
Vladimir Deribin <technician@gmx.co.uk>
2020-01-08 20:44:52 +0300

    Add files for homework terraform-1 with one star

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# set -e
+
+# Install git
+sudo apt update
+sudo apt install git -y
+
+# yankee doodle going home
+cd ~
+
+# download src
+git clone -b monolith https://github.com/express42/reddit.git
+
+# install dependencies
+cd reddit && bundle install
+
+#create systemd config
+#sudo touch /etc/systemd/system/puma.service
+#sudo chmod 664 /etc/systemd/system/puma.service
+#echo -e "[Unit]\nDescription=Puma HTTP Server\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/puma -d\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n" | sudo tee /etc/systemd/system/puma.service
+
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+
+# Reread configs
+sudo systemctl daemon-reload
+
+# Start app
+sudo systemctl start puma.service
+
+# Make autostart on boot
+sudo systemctl enable puma.service
+
+# Check status
+# sudo systemctl status puma.service
