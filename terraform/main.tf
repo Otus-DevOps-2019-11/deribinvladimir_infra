b31b4642a2dc467fa0d11c670dfbe493b75addf6
Vladimir Deribin <technician@gmx.co.uk>
2020-01-11 23:19:54 +0300

    3rd iteration - create modules app, db and vpc and parametrize it

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,23 @@ provider "google" {
   project = var.project
   region  = var.region
 }
+
+module "app" {
+  source = "./modules/app"
+  public_key_path = var.public_key_path
+  zone = var.zone
+  app_disk_image = var.app_disk_image
+}
+
+module "db" {
+  source = "./modules/db"
+  public_key_path = var.public_key_path
+  zone = var.zone
+  db_disk_image = var.db_disk_image
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+  # source_ranges = ["80.250.215.124/32"]
+  # source_ranges = ["178.66.2.16/32"]
+}
--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -6,6 +6,6 @@ resource "google_compute_firewall" "firewall_ssh" {
     protocol = "tcp"
     ports    = ["22"]
   }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["reddit-app"]
+  source_ranges = var.source_ranges
+  # target_tags = ["reddit-app","reddit-db"]
 }
