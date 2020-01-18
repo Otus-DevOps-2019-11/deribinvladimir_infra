fcfc2ff862e03ea4d9d850cb619e1d5676e2da16
Vladimir Deribin <technician@gmx.co.uk>
2020-01-18 17:47:57 +0300

    Try to use bash dynamic json script again

--- ansible/dynamicjson.sh
+++ ansible/dynamicjson.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-app_ip=`cd ../terraform/stage && terraform output | grep app_external_ip | awk '{print $3}'`
-db_ip=`cd ../terraform/stage && terraform output | grep db_external_ip | awk '{print $3}'`
+# app_ip=`cd ../terraform/stage && terraform output | grep app_external_ip | awk '{print $3}'`
+# db_ip=`cd ../terraform/stage && terraform output | grep db_external_ip | awk '{print $3}'`
 
 if [ "$1" == "--list" ] ; then
-cat<<EOF
-{"app": {"hosts": ["$app_ip"]}, "db": {"hosts": ["$db_ip"]}}
-EOF
+  echo '{"app": {"hosts": ["34.89.145.106"]}, "db": {"hosts": ["34.89.200.4"]}}'
+#cat<<EOF
+#{"app": {"hosts": ["$app_ip"]}, "db": {"hosts": ["$db_ip"]}}
+#EOF
 elif [ "$1" == "--host" ]; then
   echo '{"_meta": {"hostvars": {}}}'
 else
