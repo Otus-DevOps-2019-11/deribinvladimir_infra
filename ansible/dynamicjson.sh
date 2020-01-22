104c34ad78ff8a43321929ee01f2bfbb5ab78d50
Vladimir Deribin <technician@gmx.co.uk>
2020-01-22 13:45:17 +0300

    Try to update dynamicjson.sh again

--- ansible/dynamicjson.sh
+++ ansible/dynamicjson.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-app_ip=`cd ../terraform/stage && terraform output | grep app_external_ip | awk '{print $3}'`
-db_ip=`cd ../terraform/stage && terraform output | grep db_external_ip | awk '{print $3}'`
+# app_ip=`cd ../terraform/stage && terraform output | grep app_external_ip | awk '{print $3}'`
+# db_ip=`cd ../terraform/stage && terraform output | grep db_external_ip | awk '{print $3}'`
 
 if [ "$1" == "--list" ] ; then
   echo '{"app": {"hosts": ["34.89.145.106"]}, "db": {"hosts": ["34.89.200.4"]}}'
