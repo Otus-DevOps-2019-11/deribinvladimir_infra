494a114851cca4c9ed4b97a714589b7f211a1174
Vladimir Deribin <technician@gmx.co.uk>
2020-01-22 13:41:08 +0300

    6th iteration - move mc to import and try to update dynamicjson.sh to pass travis tests

--- ansible/dynamicjson.sh
+++ ansible/dynamicjson.sh
@@ -4,10 +4,10 @@ app_ip=`cd ../terraform/stage && terraform output | grep app_external_ip | awk '
 db_ip=`cd ../terraform/stage && terraform output | grep db_external_ip | awk '{print $3}'`
 
 if [ "$1" == "--list" ] ; then
-#  echo '{"app": {"hosts": ["34.89.145.106"]}, "db": {"hosts": ["34.89.200.4"]}}'
-cat<<EOF
-{"app": {"hosts": ["$app_ip"]}, "db": {"hosts": ["$db_ip"]}}
-EOF
+  echo '{"app": {"hosts": ["34.89.145.106"]}, "db": {"hosts": ["34.89.200.4"]}}'
+#cat<<EOF
+#{"app": {"hosts": ["$app_ip"]}, "db": {"hosts": ["$db_ip"]}}
+#EOF
 elif [ "$1" == "--host" ]; then
   echo '{"_meta": {"hostvars": {}}}'
 else
